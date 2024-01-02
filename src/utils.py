99ddbe7eac38e2010e828db37ca041f661e08f94
Olga Aukhadieva <sarole4ka@gmail.com>
2024-01-02 09:55:12 +0500

    Add sort_operations func

--- src/utils.py
+++ src/utils.py
@@ -17,3 +17,21 @@ def load_operations():
     with open('/Users/dns/PycharmProjects/Bank_Account_Transactions/src/operations.json') as file:
         operations = json.load(file)
         return operations
+
+
+def sort_operations():
+    """
+    Сортирует даты в порядке убывания.
+    Возвращает список словарей из пяти операций, отфильтрованных
+    по дате (по убыванию) и со статусом EXECUTED.
+    """
+    operations_list = []
+    for operation in load_operations():
+        try:
+            if operation['state'] == 'EXECUTED':
+                operations_list.append(operation)
+        except KeyError:
+            pass
+    sorted_list_ = sorted(operations_list, key=lambda x: datetime.strptime(x['date'], '%Y-%m-%dT%H:%M:%S.%f'),
+                          reverse=True)
+    return sorted_list_
