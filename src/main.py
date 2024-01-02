fcc5d0991e1c43eced7ae62922566d94138a0288
Olga Aukhadieva <sarole4ka@gmail.com>
2024-01-02 14:39:19 +0500

    Fix show_transactions func (main.py)

--- src/main.py
+++ src/main.py
@@ -9,8 +9,9 @@ def show_transactions():
     <откуда> -> <куда>
     <сумма перевода> <валюта>.
     """
-    sort_operations_ = sort_operations()[0:int(input('Привет! Выбери количество операций, '
-                                                     'информацию по которым ты хочешь просмотреть:\n'))]
+    # sort_operations_ = sort_operations()[0:int(input('Привет! Выбери количество операций, '
+    #                                                  'информацию по которым ты хочешь просмотреть:\n'))]
+    sort_operations_ = sort_operations()[0:5]
     for date, description, from_, to_, amount, name in zip(change_date(sort_operations_), return_description(),
                                                            hide_from(), hide_to(), return_amount(), return_name()):
         print(f'{date} {description}\n{from_} --> {to_}\n{amount} {name}\n')
